b4c0f25fbc0f98bc2b6a84de9db90808504932ab
Steven Valentino <huangwieyang@gmail.com>
2022-06-02 15:49:42 +0700

    Update about.blade.php

--- resources/views/Blog/about.blade.php
+++ resources/views/Blog/about.blade.php
@@ -31,45 +31,45 @@
                         <div class="card" style="width: 18rem;">
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
-                              <h5 class="card-title">Kategory post</h5>
-                              <p class="card-text">Isi post</p>
-                              <a href="#" class="btn btn-primary">Delete post</a>
+                                <h5 class="card-title">Kategory post</h5>
+                                <p class="card-text">Isi post</p>
+                                <a href="#" class="btn btn-primary">Delete post</a>
                             </div>
-                          </div>
+                        </div>
                     </div>
                     {{-- 2 --}}
                     <div class="col-lg-6 mt-4">
                         <div class="card" style="width: 18rem;">
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
-                              <h5 class="card-title">Kategory post</h5>
-                              <p class="card-text">Isi post</p>
-                              <a href="#" class="btn btn-primary">Delete post</a>
+                                <h5 class="card-title">Kategory post</h5>
+                                <p class="card-text">Isi post</p>
+                                <a href="#" class="btn btn-primary">Delete post</a>
                             </div>
-                          </div>
+                        </div>
                     </div>
                     {{-- 3 --}}
                     <div class="col-lg-6 mt-4">
                         <div class="card" style="width: 18rem;">
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
-                              <h5 class="card-title">Kategory post</h5>
-                              <p class="card-text">Isi post</p>
-                              <a href="#" class="btn btn-primary">Delete post</a>
+                                <h5 class="card-title">Kategory post</h5>
+                                <p class="card-text">Isi post</p>
+                                <a href="#" class="btn btn-primary">Delete post</a>
                             </div>
-                          </div>
+                        </div>
                     </div>
                     {{-- End loop --}}
                 </div>
             </div>
             <div class="col-lg-4">
-                <div class="card" style="width: 18rem;">
+                <div class="card mt-4" style="width: 18rem;">
                     <img src="..." class="card-img-top" alt="...">
                     <div class="card-body">
-                      <p class="card-text">Nama email dll</p>
-                      <a href="#" class="card-link">Edit Profile</a>
+                        <p class="card-text">Nama: <br>Email: dll</p>
+                        <a href="#" class="card-link">Edit Profile</a>
                     </div>
-                  </div>
+                </div>
             </div>
         </div>
 
