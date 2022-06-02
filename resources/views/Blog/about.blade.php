d6cb863ccd8a64e4fff3dcece8d502089d4232e0
Steven Valentino <huangwieyang@gmail.com>
2022-06-02 09:41:11 +0700

    .

--- resources/views/Blog/about.blade.php
+++ resources/views/Blog/about.blade.php
@@ -1,85 +1,85 @@
 @extends('layouts.template')
 @section('content')
-  <!-- Page Content -->
-  <!-- Banner Starts Here -->
-  <div class="heading-page header-text">
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="heading-page header-text">
     <section class="page-heading">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="text-content">
-              <h4>about us</h4>
-              <h2>more about us!</h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-content">
+                        <h4>about us</h4>
+                        <h2>more about us!</h2>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </section>
-  </div>
+</div>
 
-  <!-- Banner Ends Here -->
+<!-- Banner Ends Here -->
 
-  <section class="about-us">
+<section class="about-us">
     <div class="container">
-      <!-- Data Diri -->
-      <section>
-        <div class="card-deck">
-          <div class="card">
-            <img src="..." class="card-img-top" alt="#kenziefubrianto">
-            <div class="card-body">
-              <h5 class="card-title">Kenzie Fubrianto</h5>
-              <p class="card-text">211402139</p>
+        <!-- Data Diri -->
+        <section>
+            <div class="card-deck">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="#kenziefubrianto">
+                    <div class="card-body">
+                        <h5 class="card-title">Kenzie Fubrianto</h5>
+                        <p class="card-text">211402139</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="#mariaanggaraininatio">
+                    <div class="card-body">
+                        <h5 class="card-title">Maria Anggraini Natio</h5>
+                        <p class="card-text">211402052</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="mariyosyahraza">
+                    <div class="card-body">
+                        <h5 class="card-title">M. Ariyo Syahraza</h5>
+                        <p class="card-text">211402055</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="images/steven.jpg" class="card-img-top" alt="#stevenvalentino">
+                    <div class="card-body">
+                        <h5 class="card-title">Steven Valentino</h5>
+                        <p class="card-text">211402127 <br> 14 Februari 2004</p>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="card">
-            <img src="..." class="card-img-top" alt="#mariaanggaraininatio">
-            <div class="card-body">
-              <h5 class="card-title">Maria Anggraini Natio</h5>
-              <p class="card-text">211402052</p>
-            </div>
-          </div>
-          <div class="card">
-            <img src="..." class="card-img-top" alt="mariyosyahraza">
-            <div class="card-body">
-              <h5 class="card-title">M. Ariyo Syahraza</h5>
-              <p class="card-text">211402055</p>
-            </div>
-          </div>
-          <div class="card">
-            <img src="images/steven.jpg" class="card-img-top" alt="#stevenvalentino">
-            <div class="card-body">
-              <h5 class="card-title">Steven Valentino</h5>
-              <p class="card-text">211402127 <br> 14 Februari 2004</p>
-            </div>
-          </div>
-        </div>
-      </section>
-      <!-- End Data Diri -->
+        </section>
+        <!-- End Data Diri -->
 
-      <div class="row mt-3">
-        <div class="col-lg-3">
-          <ul class="social-icons">
-            <li><a href="https://www.instagram.com/kenzie_fu_/"><i class="fa fa-instagram"></i></a></li>
-          </ul>
+        <div class="row mt-3">
+            <div class="col-lg-3">
+                <ul class="social-icons">
+                    <li><a href="https://www.instagram.com/kenzie_fu_/"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3">
+                <ul class="social-icons">
+                    <li><a href="https://www.instagram.com/maria.natio/"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3">
+                <ul class="social-icons">
+                    <li><a href="https://www.instagram.com/ariyo_syahraza/"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3">
+                <ul class="social-icons">
+                    <li><a href="https://www.instagram.com/steven.vlntino/"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
         </div>
-        <div class="col-lg-3">
-          <ul class="social-icons">
-            <li><a href="https://www.instagram.com/maria.natio/"><i class="fa fa-instagram"></i></a></li>
-          </ul>
-        </div>
-        <div class="col-lg-3">
-          <ul class="social-icons">
-            <li><a href="https://www.instagram.com/ariyo_syahraza/"><i class="fa fa-instagram"></i></a></li>
-          </ul>
-        </div>
-        <div class="col-lg-3">
-          <ul class="social-icons">
-            <li><a href="https://www.instagram.com/steven.vlntino/"><i class="fa fa-instagram"></i></a></li>
-          </ul>
-        </div>
-      </div>
     </div>
-  </section>
+</section>
 
 
 @endsection
--- resources/views/Blog/createpost.blade.php
+++ resources/views/Blog/createpost.blade.php
@@ -1,110 +1,96 @@
 @extends('layouts.template')
 @section('content')
 
-  <!-- Page Content -->
-  <!-- Banner Starts Here -->
-  <div class="heading-page header-text">
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="heading-page header-text">
     <section class="page-heading">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="text-content">
-              <h4>Create Post</h4>
-              <h2>Hi,{{ auth()->user()->name }}</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
-
-  <!-- Banner Ends Here -->
-  <div class="row-mt-3">
-      <div class="col-md-8">
-          <div class="card">
-              <div class="card-header">
-                  <div class="d-flex align-items-center flex-shrink-0 me-3">
-                      <div class="mx-3">
-                          <img width="50px" src="assets/img/icon.png" alt="">
-                        </div>
-                        <div class="d-flex flex-column fw-bold">
-                            <a class="text-dark mb-1" >{{ auth()->user()->name }}</a>
-                            <div class="small text-muted">member</div>
-                        </div>
-                    </div>            
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-content">
+                        <h4>Create Post</h4>
+                        <h2>Hi,{{ auth()->user()->name }}</h2>
+                    </div>
                 </div>
-                <form action=" {{ route('createpost') }}" method="POST" enctype="multipart/form-data">
-                    @csrf
+            </div>
+        </div>
+    </section>
+</div>
+
+<!-- Banner Ends Here -->
+<div class="mt-4">
+    <div class="col-12 col-lg-6">
+        <div class="card">
+            <div class="card-header">
+                <div class="d-flex align-items-center flex-shrink-0 me-3">
+                    <div class="mx-3">
+                        <img width="50px" src="assets/img/icon.png" alt="">
+                    </div>
+                    <div class="d-flex flex-column fw-bold">
+                        <a class="text-dark mb-1">{{ auth()->user()->name }}</a>
+                        <div class="small text-muted">member</div>
+                    </div>
+                </div>
+            </div>
+            <form action=" {{ route('createpost') }}" method="POST" enctype="multipart/form-data">
+                @csrf
                 <div class="card-body">
                     <div class="form-group">
-                        
-                    <label for="judul">Judul Post</label>
-                    <input type="text" class="form-control" name="title" value="{{ old('title') }}" id="judul" placeholder="Tulis judul.!" required>
+                        <label for="judul">Judul Post</label>
+                        <input type="text" class="form-control" name="title" value="{{ old('title') }}" id="judul"
+                            placeholder="Tulis judul.!" required>
+                    </div>
+                    @error('title')
+                    <div style="color: red">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                    <hr>
+                    <div class="form-group">
+                        <label for="thumb">Tambah Thumbnail</label>
+                        <input type="file" class="form-control-file" name="thumb" id="thumb"
+                            placeholder="Upload thumbnail anda!" required>
+                    </div>
+                    @error('thumb')
+                    <div style="color: red">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                    <hr>
+                    <div class="form-group">
+                        <textarea class="ckeditor form-control" id="text-tab"
+                            name="content">{{ old('content') }}</textarea>
+                    </div>
+                    @error('content')
+                    <div style="color: red">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
-                @error('title')
-                <div  style="color: red">
-                    {{ $message }}
-
-                </div>
-                @enderror
-                <hr>
-                <div class="form-group">
-                    
-                    <label for="thumb">Tambah Thumbnail</label>
-                    <input type="file" class="form-control-file" name="thumb"  id="thumb" placeholder="Upload thumbnail anda!" required >
-                </div>
-                @error('thumb')
-                <div style="color: red" >
-                    {{ $message }}
-
-                </div>
-                @enderror
-                <hr>
-                <div class="form-group" >
-                    <textarea class="ckeditor form-control"id="text-tab" name="content">{{ old('content') }}</textarea>
-                </div>
-                @error('content')
-                <div  style="color: red">
-                    {{ $message }}
-
-                </div>
-                @enderror
-
-            </div>
+            </form>
         </div>
     </div>
- 
-    
-    <div class="col-6 col-md-4">
+
+    <div class="col-12 col-lg-6">
         <div class="card">
             <div class="card-body">
                 <label for="kategori">Pilih Kategori</label>
-                <select name="category_id">
+                <select name="category_id" class="ml-2">
                     @foreach ($categories as $category)
                     <option value="{{ $category->id }}">{{ $category->name }}</option>
                     @endforeach
                 </select>
-                <button class="btn btn-success btn-block mt-2" type="submit">POST</button>		
+                <button class="btn btn-success btn-block mt-2" type="submit">POST</button>
             </div>
         </div>
     </div>
 </div>
 
-</form>
-
-
-
-
-
-</div>
-</div>
-</div>
-</div>
-</section>
 
 <script src="ckeditor/ckeditor.js"></script>
-  <script>
+<script>
     CKEDITOR.replace('text-tab',{tabSpaces:4});
- </script>
+</script>
 
 @endsection
