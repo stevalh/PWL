42674a80fb976280af84de26aff217122952bc33
Steven Valentino <huangwieyang@gmail.com>
2022-06-02 10:49:19 +0700

    dropdown log out

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -33,27 +33,20 @@
             <li class="nav-item">
                 <a class="nav-link" href="/about">My Blogs</a>
             </li>
-            <li>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                <button type="submit">Log out</button>
-            </form>
-            </li>
+
             <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"  role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
                     {{ Auth::user()->name }}
                 </a>
-
-                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ route('logout') }}"onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <form class="dropdown-item" id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                         @csrf
+                        <a class="dropdown-item" href="{{ route('logout') }}"onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
                     </form>
                 </div>
-            </li>
+              </li>
          @endguest
           </ul>
         </div>
