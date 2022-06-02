511cdfe40a2916f3a64aa15ba51bce6d2cf196bb
Steven Valentino <huangwieyang@gmail.com>
2022-06-02 15:46:52 +0700

    .

--- resources/views/Blog/about.blade.php
+++ resources/views/Blog/about.blade.php
@@ -1,85 +1,83 @@
 @extends('layouts.template')
 @section('content')
-  <!-- Page Content -->
-  <!-- Banner Starts Here -->
-  <div class="heading-page header-text">
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="heading-page header-text">
     <section class="page-heading">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="text-content">
-              <h4>about us</h4>
-              <h2>more about us!</h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-content">
+                        <h4>My Blogs</h4>
+                        <h2>see all the posts you made!</h2>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </section>
-  </div>
+</div>
 
-  <!-- Banner Ends Here -->
+<!-- Banner Ends Here -->
 
-  <section class="about-us">
+<section class="about-us">
     <div class="container">
-      <!-- Data Diri -->
-      <section>
-        <div class="card-deck">
-          <div class="card">
-            <img src="..." class="card-img-top" alt="#kenziefubrianto">
-            <div class="card-body">
-              <h5 class="card-title">Kenzie Fubrianto</h5>
-              <p class="card-text">211402139</p>
+        <div class="row">
+            <div class="col-lg-8">
+                <h3>All Post You Made</h3>
+                <div class="row ">
+                    {{-- Start loop here --}}
+                    {{-- 1 --}}
+                    <div class="col-lg-6 mt-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">Kategory post</h5>
+                              <p class="card-text">Isi post</p>
+                              <a href="#" class="btn btn-primary">Delete post</a>
+                            </div>
+                          </div>
+                    </div>
+                    {{-- 2 --}}
+                    <div class="col-lg-6 mt-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">Kategory post</h5>
+                              <p class="card-text">Isi post</p>
+                              <a href="#" class="btn btn-primary">Delete post</a>
+                            </div>
+                          </div>
+                    </div>
+                    {{-- 3 --}}
+                    <div class="col-lg-6 mt-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">Kategory post</h5>
+                              <p class="card-text">Isi post</p>
+                              <a href="#" class="btn btn-primary">Delete post</a>
+                            </div>
+                          </div>
+                    </div>
+                    {{-- End loop --}}
+                </div>
             </div>
-          </div>
-          <div class="card">
-            <img src="..." class="card-img-top" alt="#mariaanggaraininatio">
-            <div class="card-body">
-              <h5 class="card-title">Maria Anggraini Natio</h5>
-              <p class="card-text">211402052</p>
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Nama email dll</p>
+                      <a href="#" class="card-link">Edit Profile</a>
+                    </div>
+                  </div>
             </div>
-          </div>
-          <div class="card">
-            <img src="..." class="card-img-top" alt="mariyosyahraza">
-            <div class="card-body">
-              <h5 class="card-title">M. Ariyo Syahraza</h5>
-              <p class="card-text">211402055</p>
-            </div>
-          </div>
-          <div class="card">
-            <img src="images/steven.jpg" class="card-img-top" alt="#stevenvalentino">
-            <div class="card-body">
-              <h5 class="card-title">Steven Valentino</h5>
-              <p class="card-text">211402127 <br> 14 Februari 2004</p>
-            </div>
-          </div>
         </div>
-      </section>
-      <!-- End Data Diri -->
 
-      <div class="row mt-3">
-        <div class="col-lg-3">
-          <ul class="social-icons">
-            <li><a href="https://www.instagram.com/kenzie_fu_/"><i class="fa fa-instagram"></i></a></li>
-          </ul>
-        </div>
-        <div class="col-lg-3">
-          <ul class="social-icons">
-            <li><a href="https://www.instagram.com/maria.natio/"><i class="fa fa-instagram"></i></a></li>
-          </ul>
-        </div>
-        <div class="col-lg-3">
-          <ul class="social-icons">
-            <li><a href="https://www.instagram.com/ariyo_syahraza/"><i class="fa fa-instagram"></i></a></li>
-          </ul>
-        </div>
-        <div class="col-lg-3">
-          <ul class="social-icons">
-            <li><a href="https://www.instagram.com/steven.vlntino/"><i class="fa fa-instagram"></i></a></li>
-          </ul>
-        </div>
-      </div>
+        <!-- Profile -->
+        <!-- End Profile -->
+
     </div>
-  </section>
+</section>
 
 
 @endsection
