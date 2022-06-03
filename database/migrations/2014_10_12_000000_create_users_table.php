4ad226fa4afb1d90121d7c0401e0489baff0411a
Marianatio <natiomaria@gmail.com>
2022-06-03 23:25:17 +0700

    penambahan kolom level table post

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('image')->default('images/default.jpeg');
-            $table->string('level')->default('number');
+            $table->string('level')->default('member');
             $table->timestamps();
         });
     }
