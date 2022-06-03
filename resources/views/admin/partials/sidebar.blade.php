1dc51d60ea5120c4f5a45db096804837d6dd0ef1
Steven Valentino <huangwieyang@gmail.com>
2022-06-03 16:40:49 +0700

    layout format admin page

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,61 @@
+<nav id="sidebar" class="sidebar js-sidebar">
+    <div class="sidebar-content js-simplebar">
+        <a class="sidebar-brand" href="/admin-panel">
+            <span class="align-middle">Admin Panel</span>
+        </a>
+
+        <ul class="sidebar-nav">
+            <li class="sidebar-header">
+                Pages
+            </li>
+
+            <li class="sidebar-item active">
+                <a class="sidebar-link" href="/admin-panel">
+                    <i class="align-middle" data-feather="sliders"></i> <span
+                        class="align-middle">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="/pagesprofile">
+                    <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="/makepost">
+                    <i class="align-middle" data-feather="check-square"></i> <span
+                        class="align-middle">Posting</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="/addcategory">
+                    <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">
+                        Add Category</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="/makecategory">
+                    <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Category</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="/cekkomentar">
+                    <i class="align-middle" data-feather="align-left"></i> <span
+                        class="align-middle">Comment</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="/approvepost">
+                    <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Approve
+                        Post</span>
+                </a>
+            </li>
+
+        </ul>
+    </div>
+</nav>
