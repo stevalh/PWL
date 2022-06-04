f66441b4305a5c06ede4068236be197a4d240d44
Steven Valentino <huangwieyang@gmail.com>
2022-06-04 17:56:33 +0700

    Update sidebar.blade.php

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -9,7 +9,7 @@
                 Pages
             </li>
 
-            <li class="sidebar-item active">
+            <li class="sidebar-item">
                 <a class="sidebar-link" href="/admin-panel">
                     <i class="align-middle" data-feather="sliders"></i> <span
                         class="align-middle">Dashboard</span>
